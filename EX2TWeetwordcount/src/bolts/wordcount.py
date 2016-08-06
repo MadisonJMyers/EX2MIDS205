617280bac91722e08e695dc356eb2f6a99c37609
Madison J Myers <mjmyers@us.ibm.com>
2016-08-06 14:10:03 -0700

    Update wordcount.py
    Updating wordcount.py to count

--- EX2TWeetwordcount/src/bolts/wordcount.py
+++ EX2TWeetwordcount/src/bolts/wordcount.py
@@ -11,36 +11,24 @@ class WordCounter(Bolt):
     def initialize(self, conf, ctx):
         self.counts = Counter()
         self.redis = StrictRedis()
-        conn = psycopg2.connect(database="tcount", user="postgres", password="password", host="localhost", port="5432")
-        cur = conn.cursor()
-        cur.execute("SELECT word FROM Tweetwordcount")
-        existing_words = cur.fetchall()
-        self.counts.update([x[0] for x in existing_words])
-
-
 
     def process(self, tup):
         word = tup.values[0]
-        conn = psycopg2.connect(database="tcount", user="postgres", password="password", host="localhost", port="5432")
-        cur = conn.cursor()
-        if self.counts[word] == 0:
-            self.counts[word] += 1 #Increament the local count
-            string = cur.mogrify("INSERT INTO Tweetwordcount VALUES (%s, %s)", (word, 1))
-            cur.execute(string)
+        if(self.counts[word] == 0 ):
+            conn = psycopg2.connect(database="tcount", user="postgres", password="password", host="localhost", port="5432")
+            cur = conn.cursor()
+            cur.execute("INSERT INTO Tweetwordcount (word, count) VALUES (%s, %s)", (word, str(1)));
             conn.commit()
+            conn.close
         else:
-            self.counts[word] += 1 # Increment the local count
-            string = cur.mogrify("UPDATE Tweetwordcount SET count=%s WHERE word=%s",(self.counts[word], word))
-            cur.execute(string)
+            conn = psycopg2.connect(database="tcount", user="postgres", password="password", host="localhost", port="5432")
+            cur = conn.cursor()
+            uCount = self.counts[word] + 1
+            cur.execute("UPDATE Tweetwordcount SET count=%s WHERE word=%s",(uCount, word))
             conn.commit()
-
-        # Write codes to increment the word count in Postgres
-        # Database name: Tcount 
-        # Table name: Tweetwordcount 
-        # you need to create both the database and the table in advance.
-        
-
-        
+            conn.close()
+ 
+        self.counts[word] += 1
         self.emit([word, self.counts[word]])
 
         # Log the count - just to see the topology running
